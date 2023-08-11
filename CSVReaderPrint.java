aa196b98f2cd652a86cb32111be93932c685fc2e
OriCohen85 <141099718+OriCohen85@users.noreply.github.com>
2023-08-11 12:19:39 -0700

    Add files via upload
    new version with reservation adding for csv files

--- CSVReaderPrint.java
+++ CSVReaderPrint.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
 
 public class CSVReaderPrint 
 {
     public void PrintReservations()
     {
+        
         String line = "";
         String splitBy = ",";
         boolean firstLineSkipped = false; 
@@ -26,5 +29,46 @@ public class CSVReaderPrint
         } catch (IOException e) {
             e.printStackTrace();
         }
+    } 
+
+    public void AddReservation(String Name, String Contact, int Guests, int RoomNum, int RoomType, String CheckInDate, String CheckInTime, String CheckOutDate, String CheckOutTime, Double Fees, Double Discount, String CheckInOutStatus){
+        Double Total = Fees - Discount;
+        String csvFile = "Reservations.csv";
+        String delimiter = ","; 
+
+        try (FileWriter writer = new FileWriter(csvFile, true)) {
+            // Appending a new line of data
+            writer.append(Name).append(delimiter)
+                    .append(Contact).append(delimiter)
+                    .append(Integer.toString(Guests)).append(delimiter)
+                    .append(Integer.toString(RoomNum)).append(delimiter)
+                    .append(Integer.toString(RoomType)).append(delimiter)
+                    .append(CheckInDate).append(delimiter)
+                    .append(CheckInTime).append(delimiter)
+                    .append(CheckOutDate).append(delimiter)
+                    .append(CheckOutTime).append(delimiter)
+                    .append("$"+Double.toString(Fees)).append(delimiter)
+                    .append("$"+Double.toString(Discount)).append(delimiter)
+                    .append("$"+Double.toString(Total)).append(delimiter)
+                    .append(CheckInOutStatus).append("\n");
+
+            System.out.println("New line added to the CSV file!");
+
+        } catch (IOException e) {
+            System.out.println("Fail!");
+            e.printStackTrace();
+
+        }
     }
+
 }
+
+
+/*
+ * How to Call methods
+ * CSVReaderPrint S = new CSVReaderPrint();
+        S.AddReservation("Bob Cohen","Bob.Cohen@gmail.com",7,401,4,"9/19/2023","2:00 PM","09/23/23","11:00 AM",1900.00,0.00,"No");
+
+   CSVReaderPrint G = new CSVReaderPrint();
+        G.PrintReservations();
+ */
