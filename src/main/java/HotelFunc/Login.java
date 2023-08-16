1597e9af6960f83f3dcc7ab8271661abae818490
Eddy Ceniza <eduardo.ceniza.875@my.csun.edu>
2023-08-15 19:25:14 -0700

    committ 2 EC

--- src/main/java/HotelFunc/Login.java
+++ src/main/java/HotelFunc/Login.java
@@ -1,12 +1,20 @@
 package HotelFunc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import HotelModel.Employee;
 
 public class Login {
     
 
 public class User {
+    
+    Employee employee = new Employee();
+    Manger manager = new Manger();
     private int id;
     private String username;
     private String password;
@@ -18,6 +26,33 @@ public class User {
         this.password = password;
     }
 }
+
+
+ public void PrintReservations()
+    {
+        
+        String line = "";
+        String splitBy = ",";
+        boolean firstLineSkipped = false; 
+
+        try {
+            
+            BufferedReader br = new BufferedReader(new FileReader("Reservations.csv"));
+
+            while ((line = br.readLine()) != null) { //skips first line in the csv file
+                if (!firstLineSkipped) {
+                    firstLineSkipped = true;
+                    continue; 
+                }
+
+                String[] reservation = line.split(splitBy);
+                System.out.println("Customer's Name: " + reservation[0] + ", Contact: " + reservation[1] + ", Guest(s): " + reservation[2] + ", Room Number: " + reservation[3] + ", Room Type: " + reservation[4] + ", Check In Date: " + reservation[5] + ", Check In Time: " + reservation[6] + ", Check Out Date: " + reservation[7] + ", Check Out Time: " + reservation[8] + ", Fees: " + reservation[9] + ", Discount: " + reservation[10] + ", Total: " + reservation[11] + ", Check in/out status: " + reservation[12]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } 
+
 
  private Map<String, User> users = new HashMap<>();
 
